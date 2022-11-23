b8d9a8a1313cef70b06ac4f35db59ef2c890227f
sadman-shami <srahyme413@gmail.com>
2022-11-23 23:50:55 +0600

    container utility setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         sans: ["Inter", "sans-serif"],
       },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1024px",
+          xl: "1024px",
+          "2xl": "1024px",
+        },
+      },
     },
   },
   plugins: [],
