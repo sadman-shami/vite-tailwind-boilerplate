205838929ba9fb5b7ed826e0b367bd91892ee51a
sadman-shami <srahyme413@gmail.com>
2022-11-24 00:12:52 +0600

    custom colors implement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html, css, js}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
@@ -14,6 +15,13 @@ module.exports = {
           xl: "1024px",
           "2xl": "1024px",
         },
+      },
+      colors: {
+        "custom-black": "#242424",
+        "custom-black-2": "#2F2F2F",
+        "custom-purple": "#646CFF",
+        "custom-gray": "#EBEBEB99",
+        "custom-white": "#FFFFFFDE",
       },
     },
   },
