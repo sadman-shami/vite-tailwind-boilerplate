5efb9befea8c5a334bab62c9d1c768337672dcbf
sadman-shami <srahyme413@gmail.com>
2022-11-24 10:25:45 +0600

    some changes applied

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html, css, js}"],
-  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter", "sans-serif"],
+        // add more fonts ...name: [...source-of-font]
       },
       container: {
         center: true,
@@ -22,8 +22,11 @@ module.exports = {
         "custom-purple": "#646CFF",
         "custom-gray": "#EBEBEB99",
         "custom-white": "#FFFFFFDE",
+        // ...color-name: color-code (hex/hsl/rgb/rgba/hsla...)
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // add your tailwind plugins
+  ],
 };
